df8de44a97d85401d84e1b173b399395ed387473
xulihang <xulihanghai@163.com>
2022-09-14 14:45:20 +0800

    use antdesign's controls and layout; load scanners to a select

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import "./App.css";
 import React from "react";
 import DocumentViewer from "./components/DWT";
 import { WebTwain } from "dwt/dist/types/WebTwain";
+import { Select, Button, Layout, Collapse  } from 'antd';
+
+const { Panel } = Collapse;
+const { Content, Sider } = Layout;
+const { Option } = Select;
 
 function App() {
+  const [scanners,setScanners] = React.useState([] as string[]);
   const dwt = React.useRef<WebTwain>();
+  const [selectedScanner,setSelectedScanner] = React.useState("");
   React.useEffect(()=>{
     console.log("load page");
   },[]);
@@ -16,18 +23,77 @@ function App() {
     }
   }
 
+  const onChange = (key: string | string[]) => {
+    console.log(key);
+  };
+
+  const onWebTWAINReady = (instance:WebTwain) => {
+    dwt.current = instance;
+    loadScannersList();
+  }
+
+  const loadScannersList = () => {
+    const DWObject = dwt.current;
+    if (DWObject) {
+      const names = DWObject.GetSourceNames(false) as string[];
+      setScanners(names);
+      if (names.length>0) {
+        setSelectedScanner(names[0]);
+      }
+    }
+  }
+
+  const onSelectedScannerChange = (value:string) => {
+    setSelectedScanner(value);
+  }
+
   return (
-    <div>
-      <div className="viewContainer">
-        <DocumentViewer
-          width="100%"
-          height="100%"
-          onWebTWAINReady={(instance)=>{dwt.current = instance}}
-        ></DocumentViewer>
-      </div>
-      <button onClick={scan}>Scan</button>
-    </div>
-    
+    <Layout hasSider>
+      <Layout style={{ marginRight: 300 }}>
+        <Content className="content">
+            <DocumentViewer
+              width="100%"
+              height="100%"
+              onWebTWAINReady={onWebTWAINReady}
+            ></DocumentViewer>
+        </Content>
+      </Layout>
+      <Sider 
+        width='300px'
+        theme='light'
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          right: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <Collapse className="controls" defaultActiveKey={['1']} onChange={onChange}>
+          <Panel header="SCAN" key="1">
+            <Select 
+              onChange={onSelectedScannerChange}
+              value={selectedScanner}
+              style={{width:"100%"}}>
+              {scanners.map(scanner => 
+                <Option 
+                  key={scanner} 
+                  value={scanner}
+                >{scanner}</Option>
+              )}
+            </Select>
+            <Button onClick={scan}>Scan</Button>
+          </Panel>
+          <Panel header="VIEWER" key="2">
+            <p>text</p>
+          </Panel>
+          <Panel header="SAVE" key="3">
+            <p>text</p>
+          </Panel>
+        </Collapse>
+      </Sider>
+  </Layout>
   );
 }
 
